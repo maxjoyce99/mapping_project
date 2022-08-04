62f947d3569d453d34770125e2339823001c5923
maxjoyce99 <maxjoyce99@gmail.com>
2022-08-04 11:39:40 -0400

    Added a component for new user registration and a button to click between them.

--- client/src/components/NewUserForm.js
+++ client/src/components/NewUserForm.js
@@ -1,34 +1,23 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 
-const Login = ({ setToken }) => {
+const NewUser = ({ setToken }) => {
 
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
+    const [email, setEmail] = useState();
 
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const token = await loginUser({
+        const newUser = await registerUser({
           username,
-          password
+          password,
+          email
         });
-        console.log(token);
-        setToken(token);
-    }
 
-    const loginUser = async(credentials) => {
-        const response = await fetch('/api/login/existing', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(credentials)
-        });
+        console.log(newUser);
         
-        const json = await response.json();
-
-        return json;
     }
 
     const registerUser = async(newUser) => {
@@ -47,8 +36,8 @@ const Login = ({ setToken }) => {
 
 
     return(
-      <div className="login-wrapper">
-        <h1>Please Log In</h1>
+      <div className="registerForm">
+        <h1>Please Create a New Account</h1>
         <form onSubmit={handleSubmit}>
           <label>
             <p>Username</p>
@@ -58,16 +47,18 @@ const Login = ({ setToken }) => {
             <p>Password</p>
             <input type="password" onChange={e => setPassword(e.target.value)} />
           </label>
+          <label>
+            <p>Email</p>
+            <input type="text" onChange={e => setEmail(e.target.value)} />
+          </label>
           <div>
-            <button type="submit"> Login </button>
+            <button type="submit"> Register </button>
           </div>
         </form>
       </div>
     )
   }
 
-  Login.propTypes = {
-    setToken: PropTypes.func.isRequired
-  }
 
-  export default Login;
+
+  export default NewUser;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,22 +1,45 @@
 import { useEffect} from 'react';
 import Login from '../components/LoginForm';
 import useToken from "../hooks/useToken";
+import NewUser from '../components/NewUserForm';
+import { useState } from 'react';
 
 const Home = () => {
     const {token, setToken } = useToken();
+    const [newUser, setNewUser] = useState(false);
+
+    const handleNewUser = () => {
+        setNewUser(!newUser);
+    }
 
     if(!token) {
-        return <Login setToken={setToken} />
+        if(newUser) {
+            return (
+                <>
+            <NewUser></NewUser>
+            <button onClick={handleNewUser}> Go Back to Login Page</button>
+            </>
+            )
+        }
+        else {
+        return (
+            <>
+        <Login setToken={setToken} />
+        <button onClick={handleNewUser}> Make a New Account</button>
+        </>
+        )
+        }
     }
-    
+    if(token){
 
     return (
             <div className="home">
 
                   <p>Max's Homepage</p> 
-                  <Login setToken={setToken}></Login>
+                  <p> Logged in as user</p>  
             </div>
         )
+    }
 
 }
 
