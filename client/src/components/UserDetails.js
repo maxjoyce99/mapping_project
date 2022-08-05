e42216c1ddfdc689f5453ee652f21d0f2f3843dc
maxjoyce99 <maxjoyce99@gmail.com>
2022-08-05 17:03:47 -0400

    Added a page to list maps from different users and a component for each users details. Also added client/public folder.

---
+++ client/src/components/UserDetails.js
@@ -0,0 +1,16 @@
+
+const UserDetails = (props) => {
+
+    
+
+    return (
+    <div>
+        <p>{props.username}</p>
+        <button>See their map!</button>
+    </div>
+    
+)
+
+}
+
+export default UserDetails;
---
+++ client/src/pages/MapList.js
@@ -0,0 +1,37 @@
+import { useEffect, useState} from 'react';
+import UserDetails from '../components/UserDetails';
+
+const MapList = () => {
+    const [userList, setUserList] = useState([]);
+
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const response = await fetch('/api/login/userlist')
+            const json = await response.json();
+
+            if(response.ok) {
+                setUserList(json);
+            }
+            else{
+                console.log("Users could not be found.");
+            }
+        }
+
+        
+        fetchUsers();
+    },[]);
+
+
+    return (
+        <div>
+        <p>Map List Page</p>
+        
+        {userList &&userList.map((user)=> (
+            <UserDetails username={user.username}></UserDetails>
+        ))}
+        </div>
+    )
+}
+
+export default MapList;
