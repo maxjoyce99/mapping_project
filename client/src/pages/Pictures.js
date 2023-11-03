75ef46593900d15ae9f616961dbcb64dc4597ad1
maxjoyce99 <maxjoyce99@gmail.com>
2023-11-03 15:37:36 -0400

    Removed unneeded comments, and print statements from pictures page.

--- client/src/pages/Pictures.js
+++ client/src/pages/Pictures.js
@@ -1,12 +1,10 @@
 import { useEffect, useState } from 'react';
 import {useLocation} from 'react-router-dom';
-import { useImageContext } from '../hooks/useImageContext';
 import useToken from "../hooks/useToken";
 import { useNavigate } from 'react-router-dom';
 
 
 const Pictures = () => {
-    //const {imagePaths, dispatchImage} = useImageContext();
     const [loading,setLoading] = useState(true);
     const {token, setToken } = useToken();
     const [pics, setPics] = useState(false);
@@ -21,7 +19,6 @@ const Pictures = () => {
             const fetchUrl = '/api/pictures/' + location.state.userId  + "/" + location.state.id;
             const response = await fetch(fetchUrl);
             const json = await response.json();
-            //set array to nothing first in case of multiple loads
             var imagePathsTemp = [];
 
             for(var i in json){
@@ -32,7 +29,6 @@ const Pictures = () => {
             if(response.ok){
             setLoading(false);
             setPics(true);
-            //dispatchImage({type: 'SET_IMAGES', payload: imagePathsTemp});
             setImagePaths(imagePathsTemp);
             }
 
@@ -60,15 +56,9 @@ const Pictures = () => {
         navigate("/modify", {state: {id: location.state.id, name: location.state.name, place: location.state.place, }});
     }
 
-    console.log("Current User ID: " + token._id);
+    /*console.log("Current User ID: " + token._id);
     console.log("User ID State: " + location.state.userId);
-    console.log("Location state ID: " + location.state.id);
-
-    
-
-    //only show edit pictures button if token._id = location.state.userId
-
-    
+    console.log("Location state ID: " + location.state.id);*/
 
     if(!loading && pics){
         return (
