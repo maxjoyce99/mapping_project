9a9443cdb0bcac407cdefa1ee9c460f7cf48c2c5
maxjoyce99 <maxjoyce99@gmail.com>
2023-11-06 12:12:42 -0500

    Added map to the home page as well as buttons to see it fullscreen and to make your own.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,31 +12,59 @@ import  example_image7 from '../images/example_image7.jpg';
 import  example_image8 from '../images/example_image8.jpg';
 import  example_map from '../images/example_map.png';
 import { useNavigate } from 'react-router';
+import Map from './Map';
+import { useLocationsContext } from '../hooks/useLocationsContext';
 
 
 const Home = () => {
 
     const navigate = useNavigate();
+    const {locations, dispatch} = useLocationsContext();
+    const {token, setToken} = useToken();
 
     const myMapClicked = () => {
       console.log("MyMapClicked");
       navigate("/map", {state: {userId: "64e653532e7d4c3970cb3dd1"}});
     }
 
+    const fetchLocations = async () => {
+      const response = await fetch('/api/locations/getall/' + "64e653532e7d4c3970cb3dd1");
+      const json = await response.json();
+
+
+      if(response.ok) {
+          console.log("Locations Found.");
+          dispatch({type: 'SET_LOCATIONS', payload: json})  
+      }
+      else{
+          console.log("Locations could not be found.");
+      }
+    }
+
+    const makeOwnClicked = () => {
+      console.log("MakeOwnClicked");
+      if(token._id =="NOUSER"){
+        navigate("/signin");
+      }
+      else{
+        navigate("/edit")
+      }
+      
+    }
+
+    useEffect(() => {
+      fetchLocations();
+    },[]);
+
     return (
       <div className="home">
+        <div className="home_map">
+        <Map></Map>
+        </div>
 
-        <div className='home_words'>
-          <h3>Where Have I Been</h3> 
-          <p>Thanks for visiting WhereHaveIBeen.com, a location based pictures sharing
-            website. Make an account and add LocationDetails and pictures, and share this with
-            your friends on the site. To see an example of how
-              I imagined this site being used click the button to see my map.
-              Below is an example of what a map could look like. With points of interest marked.
-              When you click on them you get a series of photos from that location.
-          </p>
-
-          <button onClick={myMapClicked}>My Map</button>
+        <div className='home_buttons'>
+          <button onClick={makeOwnClicked}>Make Your Own</button>
+          <button onClick={myMapClicked}>Full Screen Map</button>
         </div>
 
         <div className='example_images'>
@@ -49,7 +77,7 @@ const Home = () => {
           <img src={example_image7} className="example_image" alt="icons" ></img>
           <img src={example_image8} className="example_image" alt="icons" ></img>
         </div>
-        <img className="example_map" src={example_map}></img>
+        
       </div>
             
         )
